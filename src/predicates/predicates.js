103a1fb2e0ef7683ede1582f94b72eaf954be42a
Andrea Cadeddu <andrea.cadeddu@northwestern.edu>
2016-04-01 21:49:24 -0500

    adds wrap tap and actually export the predicates

--- src/predicates/predicates.js
+++ src/predicates/predicates.js
@@ -42,6 +42,28 @@ function both(f){
     return (x,y,...args) => f(x,...args) && f(y,...args);
 }
 
+/*
+wraps the function for sideeffects;
+shitandgiggles = p.wrap(
+  ()=>(console.log('*giggles*','a')), 
+  (stuff)=>(console.log('shit!',stuff),'b') );
+
+> shitandgiggles(123)
+shit! [ 123 ]
+*giggles* a
+
+*/
+function wrap (f, sideeffect){
+  return (...args) => (sideeffect(...args),
+    f(...args));
+}
+
+const tap = (value) =>
+ (fn) => (
+    typeof(fn) === 'function' && fn(value),
+  value )
+
+
 function allF(...funcs){
   return (...args) => funcs.reduce( (acc,x) => x.apply(null, args) && acc, true);
 }
@@ -49,3 +71,19 @@ function allF(...funcs){
 function anyF(...funcs){
   return (...args) => funcs.reduce( (acc,x) => x.apply(null, args) || acc, false);
 }
+
+module.exports = {
+    empty: empty,
+    notEmpty: notEmpty,
+    existy: existy,
+    truthy: truthy,
+    all: all,
+    any: any,
+    not: not,
+    both: both,
+    wrap:wrap,
+    tap: tap,
+    allF: allF,
+    anyF: anyF
+}
+
